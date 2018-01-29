0584514636c4c2f196f4d1495ab559b36e2f069b
ansible@amanage <yunwei@wusongtech.com>
2018-01-29 21:55:53 +0800

    update zabbix-sa

--- roles/zabbix-sa/files/zabbix-sa.sh
+++ roles/zabbix-sa/files/zabbix-sa.sh
@@ -12,3 +12,5 @@ ls -l /data/sa/index{,_law,_article}/ 2>/dev/null | grep -c '^-' > /tmp/zabbix-s
 ps -ef | grep java | grep 'SaServiceWebApplication-1.0.war' | grep -v grep | awk -F 'Xmx' '{print $2}' | awk '{print $1}' | sed 's/[kK]/000/g; s/[mM]/000000/g; s/[gG]/000000000/g' > /tmp/zabbix-sa-JvmXmx
 # tocken文件夹大小
 du -sb /data/sa/token | awk '{print $1}' > /tmp/zabbix-sa-TockenSize
+# sa状态查询
+/App/script/zabbix-sa.py
